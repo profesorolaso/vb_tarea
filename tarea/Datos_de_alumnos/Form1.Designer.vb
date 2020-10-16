939cfff3ed4963907701f38b3c8ba9d2a716273a
profesorolaso <damianolaso@gmail.com>
2020-10-16 02:07:57 -0300

    Sin Cambios

---
+++ tarea/Datos_de_alumnos/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCurso = New System.Windows.Forms.TextBox()
+        Me.txtCant = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblPromedio = New System.Windows.Forms.Label()
+        Me.lstAlumnos = New System.Windows.Forms.ListBox()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(254, 46)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(100, 49)
+        Me.btnIngresar.TabIndex = 0
+        Me.btnIngresar.Text = "Ingreso de datos de alumnos"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(79, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Curso"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Cantidad de alumnos"
+        '
+        'txtCurso
+        '
+        Me.txtCurso.Location = New System.Drawing.Point(133, 46)
+        Me.txtCurso.Name = "txtCurso"
+        Me.txtCurso.Size = New System.Drawing.Size(67, 20)
+        Me.txtCurso.TabIndex = 3
+        '
+        'txtCant
+        '
+        Me.txtCant.Location = New System.Drawing.Point(133, 68)
+        Me.txtCant.Name = "txtCant"
+        Me.txtCant.Size = New System.Drawing.Size(67, 20)
+        Me.txtCant.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Altura promedio"
+        '
+        'lblPromedio
+        '
+        Me.lblPromedio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPromedio.Location = New System.Drawing.Point(133, 98)
+        Me.lblPromedio.Name = "lblPromedio"
+        Me.lblPromedio.Size = New System.Drawing.Size(67, 35)
+        Me.lblPromedio.TabIndex = 6
+        Me.lblPromedio.Text = "Label4"
+        Me.lblPromedio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstAlumnos
+        '
+        Me.lstAlumnos.FormattingEnabled = True
+        Me.lstAlumnos.Location = New System.Drawing.Point(36, 147)
+        Me.lstAlumnos.Name = "lstAlumnos"
+        Me.lstAlumnos.Size = New System.Drawing.Size(180, 95)
+        Me.lstAlumnos.TabIndex = 7
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(254, 183)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(100, 23)
+        Me.btnSalir.TabIndex = 8
+        Me.btnSalir.Text = "&Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 261)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.lstAlumnos)
+        Me.Controls.Add(Me.lblPromedio)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCant)
+        Me.Controls.Add(Me.txtCurso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCurso As TextBox
+    Friend WithEvents txtCant As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblPromedio As Label
+    Friend WithEvents lstAlumnos As ListBox
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ tarea/Datos_de_alumnos/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private alumnos() As String
+    Private alturas() As Single
+
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim cant, i As Integer
+        Dim suma, promedio As Single
+        Me.Text = Me.Text & " de " & txtCurso.Text
+        cant = txtCant.Text
+        ReDim alumnos(cant)
+        ReDim alturas(cant)
+
+        For i = 0 To cant - 1
+            alumnos(i) = InputBox("Ingrese el Nombre del alumno:")
+            alturas(i) = InputBox("Ingrese la Altura del alumno:")
+            suma += alturas(i)        Next
+        promedio = suma / cant
+        lblPromedio.Text = promedio
+        For i = 0 To cant - 1
+            If alturas(i) > promedio Then
+                lstAlumnos.Items.Add(alumnos(i) & "-->" & alturas(i))
+            End If
+        Next
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
